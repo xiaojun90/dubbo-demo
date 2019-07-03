7d44f45cb4b6e78da1d63eae6b75520667b7dfcd
xiaojun90 <838249367@qq.com>
2019-07-03 18:07:20 +0800

    dubbo demo update

--- dubbo-demo-provider/src/main/java/com/joyxj/dubbo/demo/provider/DemoServiceImpl.java
+++ dubbo-demo-provider/src/main/java/com/joyxj/dubbo/demo/provider/DemoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.joyxj.dubbo.demo.provider;
 
 import com.joyxj.dubbo.demo.api.DemoService;
+import org.apache.dubbo.rpc.RpcContext;
 
 /**
  * 【功能描述】
@@ -12,6 +13,7 @@ import com.joyxj.dubbo.demo.api.DemoService;
 public class DemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String name) {
-        return "hello, " + name;
+        RpcContext context = RpcContext.getContext();
+        return "hello, " + name + "," +  context.getLocalHost() + "," + context.getLocalPort();
     }
 }
